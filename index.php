f44b0457984f2d24b56a693e8400c8caac539c20
Artem Majorov <root@involta.ru>
2020-04-25 16:22:05 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once(dirname(__FILE__) . "/modem.class.php");
+require_once(dirname(__FILE__) . "/library.php");
+
+$config = parse_ini_file(dirname(__FILE__) . "/.config.ini");
+$modem  = new Manoaratefy\NetworkTools\Modem($config['host'], $config['user'], $config['password']);
+
+clean_sms($modem);
+
+if (request_code($config['domen'], $config['number'])) {
+    sleep(5);
+} else {
+    exit();
+}
+
+$code = get_code($modem);
+if ($code) {
+    $access_token = get_access_token($config['domen'], $config['number'], $code);
+}
+
+
+if ($access_token) {
+    echo "$access_token\n";
+} else {
+    echo "Ошибка получения access_token\n";
+    exit();
+}
---
+++ library.php
@@ -0,0 +1,72 @@
+<?php
+
+function clean_sms($modem)
+{
+    do {
+        $stop = true;
+        $sms = $modem->smsread();
+        
+        if ($sms->Index) {
+            $modem->smsdelete($sms->Index);
+        } else {
+            $stop = false;
+        }
+        
+    } while ($stop);
+}
+
+function request_code($domen, $number) {
+    $url = "https://${domen}/api/validation/number/${number}";
+    $ch  = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, '{"sender":"Tele2"}');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $data = json_decode(curl_exec($ch), true);
+    curl_close($ch);
+    
+    if ($data != NULL) {
+        print_r($data);
+        return false;
+    }
+    
+    return true;
+}
+
+function get_code($modem) {
+    $sms = $modem->smsread();
+    
+    if (isset($sms->Content) && preg_match('/^Ваш код для входа: /', $sms->Content)) {
+        $code = preg_replace('/^Ваш код для входа: /', '', $sms->Content);
+        $code = preg_replace('/. Им можно воспользоваться только один раз.$/', '', $code);
+        return $code;
+    }
+    
+    return false;
+}
+
+function get_access_token($domen, $number, $code) {
+    $url     = "https://${domen}/auth/realms/tele2-b2c/protocol/openid-connect/token";
+    $params  = array(
+        'client_id' => 'digital-suite-web-app',
+        'grant_type' => 'password',
+        'username' => $number,
+        'password' => $code,
+        'password_type' => 'sms_code'
+    );
+    $ch      = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $data = json_decode(curl_exec($ch), true);
+    curl_close($ch);
+    
+    if (isset($data['access_token'])) {
+        return $data['access_token'];
+    }
+    
+    return false;
+}
