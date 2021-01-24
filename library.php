efa3f94695015ebb9963c4aa7c5d3b4a8fb042be
Artem Majorov <root@involta.ru>
2021-01-24 22:23:50 +0300

    update get_my_lots

--- library.php
+++ library.php
@@ -188,11 +188,16 @@ function delete_lot($number, $access_token, $id) {
 function get_my_lots($number, $access_token, $type) {
     echo date('d.m.Y H:i:s ') . "get_my_lots\n";
     
-    $lots       = false;
     $url        = "https://my.tele2.ru/api/subscribers/${number}/exchange/lots/created";
     $user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0';
     
     exec("/opt/bin/curl " . $url . " -H 'Authorization: Bearer " . $access_token . "' -H 'User-Agent: " . $user_agent . "' | /opt/bin/tr '}' '\n' | grep ',\"status\":\"active\",' -B 4 | grep -v \"^--$\" | /opt/bin/tr '\n' '}' | sed 's/^,\?/[/;s/,\?$/]/' ", $lots, $return_var);
+    
+    if ($return_var == 0) {
+        $lots = json_decode($lots[0], true);
+    } else {
+        $lots = false;
+    }
     
     return $lots;
 }
